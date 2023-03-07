b4a860d7026e1f6b72939eeee7225e8e4291cee2
Vadim <xolera200@gmail.com>
2023-03-07 19:57:12 +0200

    Lab_02

---
+++ Lab_02/read_txt/Print.Lab/Program.cs
@@ -0,0 +1,20 @@
+﻿const string path = "./read";
+
+if (!Directory.Exists(path))
+{
+    Console.WriteLine("Directory 'read' not found. Use Volume");
+    return;
+}
+
+var files = Directory.GetFiles(path).Where(x => Path.GetExtension(x) == ".txt").ToArray();
+
+
+if (files.Length == 0)
+    Console.WriteLine("Пусто");
+
+foreach (var file in files)
+{
+    Console.WriteLine(Path.GetFileName(file) + ":");
+    Console.WriteLine(File.ReadAllText(file));
+    Console.WriteLine(Environment.NewLine);
+}
